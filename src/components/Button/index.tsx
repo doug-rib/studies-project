2ef2fe76893c07578594a711c3a38a30b49ed7ef
Douglas Ribeiro <ribeiro.douglas5052@gmail.com>
2023-01-30 19:59:29 -0300

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+
+    const isActive = false;
+    const styles = { backgroundColor: isActive ? "green" : "red"}
+
+    return (
+      <button style={styles}>
+        Botão
+      </button>
+    )
+  }
+}
+
+export default Button;
